6f8049923c03a467281c73c5e6b293c802ed3d44
fatemeh khoshkam <fa.khoshkam@gmail.com>
2024-01-30 15:33:26 +0330

    first commit

---
+++ src/scripts/script.js
@@ -0,0 +1,72 @@
+/// Toggle Menu
+const btnOpenMenu = document.querySelector(".btn-open-menu");
+const btnCloseMenu = document.querySelector(".btn-close-menu");
+const aside = document.querySelector("aside");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
+
+btnOpenMenu.addEventListener("click", (event) => {
+  event.stopPropagation();
+
+  aside.classList.remove("closed");
+  body.classList.add("pointer-events-none", "overflow-hidden");
+  aside.classList.add("pointer-events-auto");
+});
+
+btnCloseMenu.addEventListener("click", (event) => {
+  aside.classList.add("closed");
+  body.classList.remove("pointer-events-none", "overflow-hidden");
+});
+
+aside.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
+document.addEventListener("click", (event) => {
+  aside.classList.add("closed");
+  body.classList.remove("pointer-events-none", "overflow-hidden");
+});
+
+/// Toggle theme (dark mode)
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+const themeToggleBtn = document.getElementById("theme-toggle");
+
+// Change the icons inside the button based on previous settings
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // if set via local storage previously
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+
+    // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/scripts/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        night: {
+          silver: "#c9c8de",
+          dark: "#111928",
+          light: "#172236",
+          lightMelo: "#2c4269",
+          lightClr: "#1f2940",
+          lightGray: "#cccccc5c",
+          lightBlue: "#79CBF3",
+          green: "#15803d",
+          red: "#b91c1c",
+          yellow: "#b9821c",
+        },
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
